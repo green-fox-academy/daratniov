a1e883b61b16e0213deb136924c1654f1fa0f171
Áron Esztergomi <aaron.esztergomi@gmail.com>
2020-03-06 03:14:50 +0100

    SubStr function done

--- week-01/day-3/Java-exercises/src/SubStr.java
+++ week-01/day-3/Java-exercises/src/SubStr.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
 
 public class SubStr {
-
     public static int subStr(String input, String q) {
-        int answer=-1;
         for (int i = 0; i < input.length(); i++) {
             if (input.charAt(i) == q.charAt(0)) {
-                answer = i;
-                for (int j = 0; j < q.charAt(q.length()); j++) {
-                    if (input.charAt(i) == q.charAt(j))
-
+                for (int j = i, k = 0; j < input.length() && k < q.length(); j++, k++) {
+                    if (input.charAt(j) != q.charAt(k)) break;
+                    if (k == q.length() - 1) return i;
                 }
             }
         }
-        return answer;
+        return -1;
     }
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Give me two strings: ");
+        System.out.println("Give me two strings!");
+        System.out.println("First string: ");
         String first = input.nextLine();
+        System.out.println("Second string: ");
         String second = input.nextLine();
         System.out.println(subStr(first, second));
     }

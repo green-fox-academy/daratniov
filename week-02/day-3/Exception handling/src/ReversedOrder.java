2242056508fa0601cd513491917cea4d0e6912f7
Áron Esztergomi <aaron.esztergomi@gmail.com>
2020-03-14 21:55:20 +0100

    Fixed codes for the first Code Review

--- week-02/day-3/Exception handling/src/ReversedOrder.java
+++ week-02/day-3/Exception handling/src/ReversedOrder.java
@@ -8,21 +8,30 @@ import java.util.List;
 public class ReversedOrder {
 
     public static void main(String[] args) {
-        reverser("assets/reversed-order.txt");
-
+        Path path = Paths.get("assets/reversed-order.txt");
+        List<String> reversedLines = new ArrayList<>();
+        fileReader(reversedLines, path);
+        fileWriter(reversedLines, path);
     }
 
-    private static void reverser(String fileName) {
-        Path path = Paths.get(fileName);
-        List<String> reversedLines = new ArrayList<>();
+    private static void fileReader(List reversedLines, Path path) {
         try {
             List<String> lines = Files.readAllLines(path);
             for (int i = 0; i < lines.size(); i++) {
                 reversedLines.add(lines.get(lines.size() - i - 1));
             }
-            Files.write(path, reversedLines);
         } catch (IOException e) {
-            System.out.println("Could not open the file!");
+            System.out.println("Could not read the file!");
+        }
+    }
+
+    private static void fileWriter(List reversedLines, Path path) {
+        try {
+            Files.write(path,reversedLines);
+        } catch (
+                IOException e) {
+            System.out.println("Could not write the file!");
         }
     }
 }
+

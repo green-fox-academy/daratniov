e6b132ef246a18f06f79a32c62304889a250f878
Áron Esztergomi <aaron.esztergomi@gmail.com>
2020-03-20 00:00:03 +0100

    Pirate exercise completed

--- week-03/day-2/ClassesAndObjects/src/pirates/Pirate.java
+++ week-03/day-2/ClassesAndObjects/src/pirates/Pirate.java
@@ -1,40 +1,63 @@
 package pirates;
 
 public class Pirate {
+  enum Status {ALIVE, DEAD, PASSED_OUT}
+
+  Status status;
   int rumCounter;
-  boolean isAlive;
-  Parrot parrot;
+  boolean parrot;
 
+  //Creates an ALIVE pirate without parrot, with a random number of drunkenness between 0 and 4
   public Pirate() {
-    rumCounter = 0;
-    isAlive = true;
+    rumCounter = (int) (Math.random() * 5);
+    status = Status.ALIVE;
+    parrot = false;
   }
 
+  //The same as above but with parrot
+  public Pirate(boolean parrot) {
+    this();
+    this.parrot = parrot;
+  }
+
+  //Drink one pint rum
+  //If reaches 4,drink another pint rum, become PASSED_OUT, and drank rum become 0
+  //If PASSED_OUT, drink a pint rum and become ALIVE
   public void drinkSomeRum() {
-    if (isAlive) {
+    if (status == Status.PASSED_OUT) {
+      status = Status.ALIVE;
       rumCounter++;
-    } else {
-      System.out.println("He is dead.");
+    } else if (status == Status.ALIVE) {
+      rumCounter++;
+      if (rumCounter > 4) {
+        status = Status.PASSED_OUT;
+        rumCounter = 0;
+      }
     }
   }
 
+  //Asks a pirate, "How is it going?"
   public void howsItGoingMate() {
-    if (isAlive && rumCounter > 4) {
-      System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
-      rumCounter = 0;
-    } else if (isAlive) {
-      System.out.println("Pour me anudder!");
-    } else {
+    if (status == Status.DEAD) {
       System.out.println("He is dead.");
+    } else if (rumCounter == 4) {
+      drinkSomeRum();
+      System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+    } else {
+      drinkSomeRum();
+      System.out.println("Pour me anudder!");
     }
   }
 
+  //Kill the pirate
   public void die() {
-    isAlive = false;
+    status = Status.DEAD;
   }
 
+  //If one is ALIVE, but the other is DEAD or PASSED_OUT, the ALIVE pirate win automatically
+  //If both of them are ALIVE, the result is 1-0 ,0-1 or 0-0 with 1/3 chance
   public void brawl(Pirate anotherPirate) {
-    if (isAlive && anotherPirate.isAlive) {
+    if (this.status == Status.ALIVE && anotherPirate.status == Status.ALIVE) {
       int chance = (int) (Math.random() * 3);
       if (chance == 0) {
         this.die();
@@ -44,6 +67,15 @@ public class Pirate {
         this.die();
         anotherPirate.die();
       }
+    } else if (this.status == Status.ALIVE) {
+      anotherPirate.die();
+    } else if (anotherPirate.status == Status.ALIVE) {
+      this.die();
     }
+  }
+
+  //Prints out the state of a pirate
+  public void stateOfAPirate() {
+    System.out.println("status: " + status + ", rum: " + rumCounter);
   }
 }
--- week-03/day-2/ClassesAndObjects/src/pirates/Ship.java
+++ week-03/day-2/ClassesAndObjects/src/pirates/Ship.java
@@ -7,65 +7,111 @@ public class Ship {
   Pirate captain;
   List<Pirate> crew;
 
+  //Creates an empty ship
   public Ship() {
     crew = new ArrayList<>();
   }
 
+  //Fill the ship with a captain, and a random number of pirates between 1 and 20
   public void fillShip() {
-    int numberOfPirates = (int) (Math.random() * 100);
-    captain = new Pirate();
+    captain = new Pirate(true);
+    int numberOfPirates = (int) (Math.random() * 20 + 1);
     for (int i = 0; i < numberOfPirates; i++) {
       crew.add(new Pirate());
     }
   }
 
-  public void stateOfTheShip() {
-    if (captain.isAlive && captain.rumCounter < 5) {
-      System.out.println("The Captain is alive!");
-    } else if (captain.isAlive) {
-      System.out.println("The Captain passed out!");
-    } else {
-      System.out.println("The Captain is dead!");
+  //Kills everyone who is PASSED_OUT in both of the ships
+  //Assign the winner and the loser ship with the given formula
+  public boolean battle(Ship anotherShip) {
+    for (Pirate pirate : this.crew) {
+      if (pirate.status == Pirate.Status.PASSED_OUT) {
+        pirate.status = Pirate.Status.DEAD;
+      }
     }
-    System.out.println("The Captain drank " + captain.rumCounter + " pint of rum!");
-    System.out.println(alivePirateCounter() + " pirate is alive!");
+    for (Pirate pirate : anotherShip.crew) {
+      if (pirate.status == Pirate.Status.PASSED_OUT) {
+        pirate.status = Pirate.Status.DEAD;
+      }
+    }
+    if (this.alivePirateCounter() - this.captain.rumCounter > anotherShip.alivePirateCounter() - anotherShip.captain.rumCounter) {
+      anotherShip.crewKiller();
+      this.celebration();
+      return true;
+    } else {
+      this.crewKiller();
+      anotherShip.celebration();
+      return false;
+    }
   }
 
+  //Counts the ALIVE pirates in the actual ship
   private int alivePirateCounter() {
     int alivePirate = 0;
     for (Pirate pirate : crew) {
-      if (pirate.isAlive) {
+      if (pirate.status == Pirate.Status.ALIVE) {
         alivePirate++;
       }
     }
     return alivePirate;
   }
 
-  public boolean battle(Ship anotherShip) {
-
-    int numberOfDeaths;
-    if (this.alivePirateCounter() - this.captain.rumCounter > anotherShip.alivePirateCounter() - anotherShip.captain.rumCounter) {
-      numberOfDeaths = (int) (Math.random() * anotherShip.alivePirateCounter());
-      for (Pirate pirate : anotherShip.crew) {
-        while (numberOfDeaths != 0) {
-          if (pirate.isAlive) {
-            pirate.die();
-            numberOfDeaths--;
-          }
-        }
+  //Kills a random number of pirates between 1 and the number of ALIVE pirates in the loser ship
+  private void crewKiller() {
+    int numberOfDeaths = (int) (Math.random() * (alivePirateCounter() + 1));
+    for (int i = 0; numberOfDeaths != 0; i++) {
+      if (crew.get(i).status == Pirate.Status.ALIVE) {
+        crew.get(i).status = Pirate.Status.DEAD;
+        numberOfDeaths--;
       }
-      return true;
-    } else {
-      numberOfDeaths = (int) (Math.random() * this.alivePirateCounter());
-      for (Pirate pirate : this.crew) {
-        while (numberOfDeaths != 0) {
-          if (pirate.isAlive) {
-            pirate.die();
-            numberOfDeaths--;
-          }
-        }
-      }
-      return false;
     }
+  }
+
+  //Everybody drinks a random number of rum between 1 and 4 in the winner ship
+  private void celebration() {
+    int numberOfRums = (int) (Math.random() * 4 + 1);
+    for (int i = 0; i < numberOfRums; i++) {
+      captain.drinkSomeRum();
+    }
+    for (Pirate pirate : crew) {
+      numberOfRums = (int) (Math.random() * 4 + 1);
+      for (int i = 0; i < numberOfRums; i++) {
+        pirate.drinkSomeRum();
+      }
+    }
+  }
+
+  //Counts the PASSED_OUT pirates in the actual ship
+  private int passedOutPirateCounter() {
+    int passedOutPirate = 0;
+    for (Pirate pirate : crew) {
+      if (pirate.status == Pirate.Status.PASSED_OUT) {
+        passedOutPirate++;
+      }
+    }
+    return passedOutPirate;
+  }
+
+  //Counts the DEAD pirates in the actual ship
+  private int deadPirateCounter() {
+    int deadPirate = 0;
+    for (Pirate pirate : crew) {
+      if (pirate.status == Pirate.Status.DEAD) {
+        deadPirate++;
+      }
+    }
+    return deadPirate;
+  }
+
+  //Prints out the status of the captain
+  public void stateOfTheCaptain() {
+    System.out.println("The captain is " + captain.status + ".");
+    System.out.println("Drank " + captain.rumCounter + " pint of rum.");
+  }
+
+  //Prints out the status of the ship
+  public void stateOfTheShip() {
+    System.out.println("ALIVE: " + alivePirateCounter() + ", PASSED OUT: " + passedOutPirateCounter()
+        + ", DEAD: " + deadPirateCounter() + ", RUM: " + captain.rumCounter);
   }
 }

f7745298a0d1cc1beb5f61eaae97cbf3a6ad3b2c
Áron Esztergomi <aaron.esztergomi@gmail.com>
2020-03-21 04:50:56 +0100

    Fibonacci exercise completed!

---
+++ week-03/day-5/Testing/src/fibonacci/Fibonacci.java
@@ -0,0 +1,21 @@
+package fibonacci;
+
+public class Fibonacci {
+  public int fibonacciCounter(int index) {
+    int sum = 0;
+    int a = 0;
+    int b = 1;
+
+    if (index <= 0) return -1;
+    else if (index == 1)return a;
+    else if (index == 2) return b;
+    else {
+      for (int i = 0; i < index - 2; i++) {
+        sum = a+b;
+        a = b;
+        b = sum;
+      }
+      return sum;
+    }
+  }
+}

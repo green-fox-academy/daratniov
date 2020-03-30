c26965d56959fd328abc4bd7da21db90470963c1
Áron Esztergomi <aaron.esztergomi@gmail.com>
2020-03-30 22:51:57 +0200

    CharSequence exercise completed!

---
+++ week-04/day-2/Abstracts & Interfaces/src/charsequence/Main.java
@@ -0,0 +1,18 @@
+package charsequence;
+
+public class Main {
+  public static void main(String[] args) {
+    String string = "example";
+    Gnirts gnirts = new Gnirts(string);
+    Shifter shifter = new Shifter(string, 2);
+    System.out.println(string.length());
+    System.out.println(gnirts.length());
+    System.out.println(shifter.length());
+    System.out.println(string.charAt(3));
+    System.out.println(gnirts.charAt(3));
+    System.out.println(shifter.charAt(3));
+    System.out.println(string.subSequence(1, 3));
+    System.out.println(gnirts.subSequence(1, 3));
+    System.out.println(shifter.subSequence(1, 3));
+  }
+}
---
+++ week-04/day-2/Abstracts & Interfaces/src/charsequence/Shifter.java
@@ -0,0 +1,38 @@
+package charsequence;
+
+import java.util.stream.IntStream;
+
+public class Shifter implements CharSequence {
+  private String string;
+  private int numberToShift;
+
+  public Shifter(String string, int numberToShift) {
+    this.string = string;
+    this.numberToShift = numberToShift;
+  }
+
+  @Override
+  public int length() {
+    return this.string.length();
+  }
+
+  @Override
+  public char charAt(int i) {
+    return this.string.charAt(i + numberToShift);
+  }
+
+  @Override
+  public CharSequence subSequence(int start, int end) {
+    return this.string.subSequence(start + numberToShift, end + numberToShift);
+  }
+
+  @Override
+  public IntStream chars() {
+    return this.string.chars();
+  }
+
+  @Override
+  public IntStream codePoints() {
+    return this.string.codePoints();
+  }
+}
